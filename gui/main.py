98b843e8e411b515c24e0268ef0d7871b7c30f18
Kroeker <stefan.kroeker@fh-bielefeld.de>
2018-12-21 12:34:37 +0100

    add threads

--- gui/main.py
+++ gui/main.py
@@ -1,12 +1,14 @@
 from gui import *
 from queueManager import QueueManager
 from serialManager import SerialManager
+import threading
 
 
 def main():
 
     queue_manager = QueueManager()
-    SerialManager(queue_manager)
+    t_serial_manager = threading.Thread(target=SerialManager, args=(queue_manager,))
+    t_serial_manager.start()
     Gui(queue_manager)
 
 
